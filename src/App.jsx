eb87888f49c9719b35ff96c41a3734bf7178a96b
carraes <carraeshb@gmail.com>
2022-10-31 21:07:01 -0300

    fix: fix links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import Timer from './components/Timer'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
-    return <div><Timer /></div>
+    return (
+      <div>
+        <Timer />
+        <Footer />
+      </div>
+    )
   }
 }
 
