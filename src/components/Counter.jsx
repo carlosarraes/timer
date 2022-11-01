0066fe2bf923838c190869f312474a29f1ed2464
carraes <carraeshb@gmail.com>
2022-11-01 10:04:09 -0300

    build: time converter, pause, restart

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { AiFillPauseCircle, AiFillPlayCircle } from 'react-icons/ai'
 
 const SECOND = 1000;
 
@@ -8,15 +9,28 @@ class Counter extends Component {
 
     this.state = {
       tempo: 0,
+      segundos: 0,
+      minutos: 0,
       error: false,
+      start: true,
     }
   }
 
-  callTimer() {
+  callTimer = () => {
     this.intervalId = setInterval(() => {
-      this.setState((prevState) => ({...prevState, tempo: prevState.tempo - 1}))
+      this.setState((prevState) => ({...prevState, tempo: prevState.tempo - 1, segundos: prevState.segundos - 1}))
     }, SECOND);
   }
+
+  pauseTimer = () => {
+    clearInterval(this.intervalId);
+    this.setState({start: false});
+  }
+
+  restartTimer = () => {
+    this.callTimer();
+    this.setState({start: true});
+  };
 
   componentDidMount() {
     const { tempo: tempoProps } = this.props
@@ -36,7 +50,10 @@ class Counter extends Component {
         countOk = true;
       }
     }
-    this.setState({tempo: convertedTempo});
+    const convertedMinutos = Math.floor(convertedTempo / 60);
+    const convertedSegundos = convertedTempo % 60;
+
+    this.setState({tempo: convertedTempo, segundos: convertedSegundos, minutos: convertedMinutos});
     
     if (countOk) return this.callTimer();
     else return this.setState({error: true});
@@ -47,26 +64,35 @@ class Counter extends Component {
   }
 
   componentDidUpdate() {
-    const { tempo } = this.state;
+    const { tempo, segundos, minutos, start } = this.state;
     if ( tempo === 0 ) {
       clearInterval(this.intervalId);
+    }
+
+    if ( segundos === 0 && minutos && start) {
+      this.setState((prevState) => ({...prevState, minutos: prevState.minutos - 1, segundos: 60}))
     }
   }
 
   render() {
-    const { tempo, error } = this.state;
+    const { tempo, minutos, segundos, error, start } = this.state;
 
     return (
       <>
         { error ?
-          <> 
+          <section> 
             <h1>Error, tente novamente.</h1> 
             <p>tempo tem q ser exatamente como o exemplo:  3m 15s</p>
-          </>:
-          <>
-            <h1 className='text-8xl font-bold'>{`${tempo}s`}</h1>
+          </section>:
+          <section className='flex flex-col justify-center items-center gap-2'>
+            <div className='flex justify-between items-center gap-6'>
+              <h1 className='text-8xl font-bold'>{`${minutos.toString().padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`}</h1>
+              { start ?
+              <AiFillPauseCircle className='cursor-pointer text-2xl' onClick={this.pauseTimer} />
+              : <AiFillPlayCircle className='cursor-pointer text-2xl' onClick={this.restartTimer} /> }
+            </div>
             {tempo === 0 && <span className='text-xl font-bold my-3'>É HEXA!</span>}
-          </>}
+          </section>}
       </>
     )
   }
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/timer',
+  base: '/timer/',
   plugins: [react()]
 })
